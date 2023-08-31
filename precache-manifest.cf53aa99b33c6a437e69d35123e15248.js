628bcac901ce0bdfafd44bb192415adc15981fc9
Noman ch <mnomanch786@gmail.com>
2023-08-31 21:08:20 +0500

    Updates

---
+++ precache-manifest.cf53aa99b33c6a437e69d35123e15248.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a88968b2cdd3b2135c8d",
+    "url": "/ReactAPP/static/css/main.2c6d2eea.chunk.css"
+  },
+  {
+    "revision": "a88968b2cdd3b2135c8d",
+    "url": "/ReactAPP/static/js/main.a88968b2.chunk.js"
+  },
+  {
+    "revision": "3a79397ac1a1b3a66e79",
+    "url": "/ReactAPP/static/js/1.3a79397a.chunk.js"
+  },
+  {
+    "revision": "1e432fc06f3d49454627",
+    "url": "/ReactAPP/static/js/2.1e432fc0.chunk.js"
+  },
+  {
+    "revision": "57acebf85c2af90a640c",
+    "url": "/ReactAPP/static/js/runtime~main.57acebf8.js"
+  },
+  {
+    "revision": "310012518c10b3214b0dd49548c80964",
+    "url": "/ReactAPP/static/media/Home_cinema.31001251.gif"
+  },
+  {
+    "revision": "77005ebf66435995c295d9976946b6c5",
+    "url": "/ReactAPP/index.html"
+  }
+];
